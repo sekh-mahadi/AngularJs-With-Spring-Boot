52b6b88e331aacfdc342ccde5bc0cc8ac5e893db
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2021-05-27 11:29:49 +0600

    Search Category and Search Options Completed.

--- spring-boot-ecommerce/src/main/java/io/mtech/ecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/io/mtech/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,14 @@
 package io.mtech.ecommerce.config;
 
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -7,8 +16,18 @@ import org.springframework.http.HttpMethod;
 
 import io.mtech.ecommerce.entity.Product;
 import io.mtech.ecommerce.entity.ProductCategory;
+
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
+
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
@@ -16,10 +35,32 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		config.getExposureConfiguration().forDomainType(Product.class)
 				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-	
-		config.getExposureConfiguration().forDomainType(ProductCategory.class)
-		.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-		.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+		config.getExposureConfiguration().forDomainType(ProductCategory.class)
+				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+		// call the internal helper method
+		exposeIds(config);
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		// expose entity ids
+        //
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
 	}
 }
