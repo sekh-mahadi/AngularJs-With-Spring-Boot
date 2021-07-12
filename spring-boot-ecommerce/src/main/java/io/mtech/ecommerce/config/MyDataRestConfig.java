85bbd2330f9b69845e8bc89aef01ae0afbf70a3d
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2021-07-12 11:08:52 +0600

    CheckOut Form: Backend code for Country and State - Completed.

--- spring-boot-ecommerce/src/main/java/io/mtech/ecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/io/mtech/ecommerce/config/MyDataRestConfig.java
@@ -9,14 +9,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import io.mtech.ecommerce.entity.Country;
 import io.mtech.ecommerce.entity.Product;
 import io.mtech.ecommerce.entity.ProductCategory;
-
+import io.mtech.ecommerce.entity.State;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
@@ -32,35 +38,49 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
 		// Disable HTTP methods for product: PUT,POST and DELETE
-		config.getExposureConfiguration().forDomainType(Product.class)
-				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-		config.getExposureConfiguration().forDomainType(ProductCategory.class)
-				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		disableHttpMethods(Product.class, config, theUnsupportedActions);
+		disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+		disableHttpMethods(Country.class, config, theUnsupportedActions);
+		disableHttpMethods(State.class, config, theUnsupportedActions);
 
 		// call the internal helper method
 		exposeIds(config);
 	}
 
+	private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config,
+			HttpMethod[] theUnsupportedActions) {
+		config.getExposureConfiguration()
+		        .forDomainType(theClass)
+				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+	}
+
 	private void exposeIds(RepositoryRestConfiguration config) {
 		// expose entity ids
-        //
+		//
 
-        // - get a list of all entity classes from the entity manager
-        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		// - get a list of all entity classes from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
-        // - create an array of the entity types
-        List<Class> entityClasses = new ArrayList<>();
+		// - create an array of the entity types
+		List<Class> entityClasses = new ArrayList<>();
 
-        // - get the entity types for the entities
-        for (EntityType tempEntityType : entities) {
-            entityClasses.add(tempEntityType.getJavaType());
-        }
+		// - get the entity types for the entities
+		for (EntityType tempEntityType : entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
 
-        // - expose the entity ids for the array of entity/domain types
-        Class[] domainTypes = entityClasses.toArray(new Class[0]);
-        config.exposeIdsFor(domainTypes);
+		// - expose the entity ids for the array of entity/domain types
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
+
+	/*
+	 * @Bean public Docket productApi() { return new
+	 * Docket(DocumentationType.SWAGGER_2)
+	 * .select().apis(RequestHandlerSelectors.basePackage("io.mtech.ecommerce.dao"))
+	 * .paths("/product.*") .build();
+	 * 
+	 * }
+	 */
 }
