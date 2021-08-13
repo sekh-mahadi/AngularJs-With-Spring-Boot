313f4a8e47b3edcc0dd119b9495588a6a2f987cb
sekh-mahadi <sekh.mahadi2007cse@gmail.com>
2021-08-13 10:44:54 +0600

    Security - LoginLogout - Develop App Configuration

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa1gjcsqozgP1VND5d7',
+        issuer: 'https://dev-59486908.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes:['openid', 'profile', 'email']
+   } 
+}
